0fc195e2511c6e939fea28b2166fb7c1be025fdf
YUSUF\yusuf <yusufmendes123@gmail.com>
2023-07-26 21:31:35 +0300

    Add question 10

---
+++ app/src/main/java/com/example/sisterslabakademiuygulamadersi/hafta1/soru10/Ornek10.kt
@@ -0,0 +1,33 @@
+package com.example.sisterslabakademiuygulamadersi.hafta1.soru10
+
+import java.util.Scanner
+
+fun main() {
+
+    /*
+    * Bir Integer türünde ArrayList oluşturun ve kullanıcıdan alınan 5 adet sayıyı bu listeye ekleyin.
+    * Ardından, listedeki sayıları toplayan bir fonksiyon yazın ve sonucu ekrana yazdırın
+    * */
+
+    val arrayList: ArrayList<Int> = arrayListOf()
+    val scanner = Scanner(System.`in`)
+    for (i in 1..5) {
+        println("Sayı girin")
+        val sayi = scanner.nextInt()
+        arrayList.add(sayi)
+    }
+
+    println("Array listesindeki değerler : ${arrayList.joinToString()}")
+
+    arrayListTopla(arrayList)
+
+}
+
+fun arrayListTopla(arrayList: ArrayList<Int>) {
+    var toplam = 0
+    for (i in 0 until arrayList.size) {
+        toplam += arrayList[i]
+    }
+
+    println("Array list toplamı: $toplam")
+}
