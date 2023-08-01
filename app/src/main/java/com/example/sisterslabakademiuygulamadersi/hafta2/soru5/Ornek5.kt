abb7b26bc760a8afdc4868f3a28c74604db72ce1
YUSUF\yusuf <yusufmendes123@gmail.com>
2023-08-01 21:10:12 +0300

    Add question five second week

---
+++ app/src/main/java/com/example/sisterslabakademiuygulamadersi/hafta2/soru5/Ornek5.kt
@@ -0,0 +1,25 @@
+package com.example.sisterslabakademiuygulamadersi.hafta2.soru5
+
+fun main() {
+
+    /*
+    * Verilen bir listedeki tekrar eden elemanları HashSet olarak döndüren bir fonksiyon yazın.
+    * */
+
+    val arrayList = listOf<Int>(1, 5, 7, 9, 1, 5, 8, 7, 2, 2, 1, 5)
+    println("Tekrar Edenler: ${hashMapDonder(arrayList)}")
+
+}
+
+fun hashMapDonder(list: List<Int>): HashSet<Int> {
+    val tekrarEtmeyen = HashSet<Int>()
+    val tekrarEdenler = HashSet<Int>()
+
+    for (i in list) {
+        if (!tekrarEtmeyen.add(i)) {
+            tekrarEdenler.add(i)
+        }
+    }
+    return tekrarEdenler
+
+}
