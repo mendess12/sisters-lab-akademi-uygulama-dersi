ac19a833ebf76e2628d7906d51a33c949def4805
YUSUF\yusuf <yusufmendes123@gmail.com>
2023-08-02 20:51:03 +0300

    Add question eleven second week

---
+++ app/src/main/java/com/example/sisterslabakademiuygulamadersi/hafta2/soru11/Ornek11.kt
@@ -0,0 +1,17 @@
+package com.example.sisterslabakademiuygulamadersi.hafta2.soru11
+
+fun main() {
+
+    /*
+    *  Verilen bir liste içerisindeki elemanların sayısını bir HashMap olarak döndüren bir fonksiyon yazın.
+    * */
+
+    val liste = listOf<Int>(10, 2, 4, 6, 2, 45, 8, 7, 4, 2, 3, 10, 2, 5, 8, 7)
+    val hashMapListe = HashMap<Int, Int>()
+    for (i in liste) {
+        if (!hashMapListe.keys.contains(i)) {
+            hashMapListe.put(i, liste.filter { it == i }.count())
+        }
+    }
+    println(hashMapListe)
+}
