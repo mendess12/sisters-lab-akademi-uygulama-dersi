03d377397ee71b9ef8514c422f1b81fd7c6468e5
YUSUF\yusuf <yusufmendes123@gmail.com>
2023-08-08 20:51:23 +0300

    Add question one week four

--- app/src/main/java/com/example/sisterslabakademiuygulamadersi/MainActivity.kt
+++ app/src/main/java/com/example/sisterslabakademiuygulamadersi/MainActivity.kt
@@ -1,11 +1,24 @@
 package com.example.sisterslabakademiuygulamadersi
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.sisterslabakademiuygulamadersi.databinding.ActivityMainBinding
+import com.example.sisterslabakademiuygulamadersi.hafta4.Ornek1
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        binding.exampleOne.setOnClickListener {
+            val exampleOneIntent = Intent(this@MainActivity, Ornek1::class.java)
+            startActivity(exampleOneIntent)
+        }
     }
 }
---
+++ app/src/main/java/com/example/sisterslabakademiuygulamadersi/hafta4/Ornek1.kt
@@ -0,0 +1,12 @@
+package com.example.sisterslabakademiuygulamadersi.hafta4
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.sisterslabakademiuygulamadersi.R
+
+class Ornek1 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ornek1)
+    }
+}
